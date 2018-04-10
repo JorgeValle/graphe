4ba6ad7030e0f8ddf2d7feb186a94b1237012c08
jvalle <jvalle@veic.org>
2018-04-09 21:13:23 -0400

    Changed asset dirname to base, so that node_modules was accessible.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'front/assets')));
+app.use(express.static(path.join(__dirname, '/')));
 
 // middleware allows for put and delete requests on forms
 app.use(methodOverride('_method'));
