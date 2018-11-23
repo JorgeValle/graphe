a539ed6f9d0856e0da183bbaf9ae6cd2bd1cf54f
Jorge Valle <jorge.e.valle@gmail.com>
2018-11-23 13:27:38 +0100

    Wrapped up documentation for /admin and /front directories.

--- admin/admin.controller.js
+++ admin/admin.controller.js
@@ -4,26 +4,34 @@ const request = require('request'),
       serverService = require('../services/server.service');
 
 /**
- * 
+ * Renders the form to update content
+ * @since 2.0.0
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @param {object} post - The post object that we will render
  */
-const renderUpdateContent = function(req, res, thisPost) {
-
+const renderUpdateForm = function(req, res, post) {
   res.render('update', {
     documentTitle: 'Update | Antares',
-    // we parse JSON response to get properties ready for consumption in pug templates
-    post: JSON.parse(thisPost)
+    // We parse JSON response to get properties ready for consumption in pug templates
+    post: JSON.parse(post)
   });
 };
 
 
 /**
- * 
+ * Renders the main query for all content
+ * @since 2.0.0
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @param {object} posts - The posts we will render
+ * @param {object} quotes - The quotes we will render
+ * @param {object} events - The events we are rendering
  */
 const renderQueryContent = function(req, res, posts, quotes, events) {
-
   res.render('query', {
     documentTitle: 'Query | Antares',
-    // we parse JSON response to get properties ready for consumption in pug templates
+    // We parse JSON response to get properties ready for consumption in pug templates
     posts: JSON.parse(posts),
     quotes: JSON.parse(quotes),
     events: JSON.parse(events)
@@ -32,8 +40,9 @@ const renderQueryContent = function(req, res, posts, quotes, events) {
 
 /**
  * Renders the create page
- * @param {*} req 
- * @param {*} res 
+ * @since 2.0.0
+ * @param {object} req - The request object
+ * @param {object} res - The response object
  */
 module.exports.create = function(req, res) {
   res.render('create', { 
@@ -42,42 +51,45 @@ module.exports.create = function(req, res) {
 };
 
 /**
- * Get all from content type
- * @param {string} res - the test value
- * @param {string} status - the status response
- * @returns
+ * Queries all content types
+ * @since 2.0.0
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @param {object} posts - The blog posts we are querying
+ * @param {object} events - The events we are querying
+ * @param {object} quotes - The quotes we are querying
  */
 module.exports.queryAll = function(req, res) {
 
-  var path = '/api/get/posts',
+  let path = '/api/get/posts',
       fullUrl = serverService.returnBaseUrl() + path,
       requestOptions = {
         url: fullUrl,
         method: 'get'
       };
 
-  // let's get the posts
+  // Let's get the posts
   request(requestOptions, function(err, response, posts) {
 
     if (err) {
       console.log('Request error' + err);
     } else {
 
-      var path = '/api/get/quotes',
+      let path = '/api/get/quotes',
           fullUrl = serverService.returnBaseUrl() + path,
           requestOptions = {
             url: fullUrl,
             method: 'get'
           };
 
-      // now let's get the quotes
+      // Now let's get the quotes
       request(requestOptions, function(err, response, quotes) {
 
         if (err) {
           renderQueryContent(req, res, posts);
         } else {
 
-          var path = '/api/get/events',
+          let path = '/api/get/events',
               fullUrl = serverService.returnBaseUrl() + path,
               requestOptions = {
                 url: fullUrl,
@@ -97,32 +109,29 @@ module.exports.queryAll = function(req, res) {
       });
     }
   });
-
 };
 
 /**
- * Updates
- * @param {*} req 
- * @param {*} res 
- * @returns
+ * Queries the single blog post we want to update
+ * @since 2.0.0
+ * @param {object} req - The request object
+ * @param {oject} res - The response object
  */
 module.exports.queryOne = function(req, res) {
 
-  var wantedSlug = req.query.post,
-      path = `/api/get/post/${wantedSlug}`,
+  let path = `/api/get/post/${req.query.post}`,
       fullUrl = serverService.returnBaseUrl() + path,
       requestOptions = {
         url: fullUrl,
         method: 'get'
       };
 
-  request(requestOptions, function(err, response, body) {
+  request(requestOptions, function(err, response, post) {
 
     if (err) {
       console.error(`Request error: ${err}`);
     } else {
-      console.log('renderUpdateContent ran');
-      renderUpdateContent(req, res, body);
+      renderUpdateForm(req, res, post);
     }
 
   });
